ef3f245fefd469a0592842f65c42e6745650d46b
Mooder <63562308+Mood-al@users.noreply.github.com>
2023-04-06 04:08:40 +0200

    adding new changes

--- pages/index.js
+++ pages/index.js
@@ -1,87 +1,79 @@
-// import React, { useCallback, useRef, useState } from "react";
-// // import "./styles/bootstrap.css";
-// // import "./styles/rkm.css";
-// // import "./styles/main.css";
+import React, { useCallback, useRef, useState } from "react";
+// import "./styles/bootstrap.css";
+// import "./styles/rkm.css";
+// import "./styles/main.css";
 
-// import { MenuBlock, Menu } from "react-kfc-menu";
-// import { Tabs, Tab } from "react-tabs-scrollable";
-// import "react-tabs-scrollable/dist/rts.css";
+import { MenuBlock, Menu } from "react-kfc-menu";
+import { Tabs, Tab } from "react-tabs-scrollable";
+import "react-tabs-scrollable/dist/rts.css";
 
-// function App() {
-//   const [activeTab, setActiveTab] = React.useState(1);
+function App() {
+  const [activeTab, setActiveTab] = React.useState(1);
 
-//   // define a onClick function to bind the value on tab click
-//   const menuRef = useRef(null);
-//   const onTabClick = (e, index) => {
-//     setActiveTab(index);
-//     menuRef.current.scrollSelectedToBlock(index);
-//     console.log(index, "onTabClick");
-//   };
+  // define a onClick function to bind the value on tab click
+  const menuRef = useRef(null);
+  const onTabClick = (e, index) => {
+    setActiveTab(index);
+    menuRef.current.scrollSelectedToBlock(index);
+    console.log(index, "onTabClick");
+  };
 
-//   const onBlockIntersection = (e, index) => {
-//     console.log(index, "onBlockIntersection");
-//     setActiveTab(index);
-//   };
-//   return (
-//     <>
-//       <div
-//         className="sticky-top bg-light"
-//         style={{ position: "sticky", top: 0, background: "white", zIndex: 99 }}
-//       >
-//         <div className="container">
-//           <Tabs activeTab={activeTab} onTabClick={onTabClick}>
-//             {/* generating an array to loop through it  */}
-//             {[...Array(20).keys()].map((item) => (
-//               <Tab key={item}>Tab {item}</Tab>
-//             ))}
-//           </Tabs>
-//         </div>
-//       </div>
-//       <div className="row mx-auto justify-content-center">
-//         <div className="col-md-9 ">
-//           <Menu
-//             onBlockIntersection={onBlockIntersection}
-//             // // containerClassName="container"
-//             // indicatorTopPostion={60}
-//             activeSection={activeTab}
-//             action={menuRef}
-//           >
-//             {[...Array(20).keys()].map((item) => (
-//               <MenuBlock key={item}>
-//                 <div className="display-4">Block {item}</div>{" "}
-//                 <div className="row">
-//                   {[...Array(8).keys()].map((card) => (
-//                     <div key={card} className="col-md-3 my-2">
-//                       <div className="card">
-//                         <div className="card-body">
-//                           {card} Lorem ipsum dolor sit amet consectetur,
-//                           adipisicing elit. Modi deleniti natus voluptates
-//                           doloribus voluptate voluptas ab eum dolorem asperiores
-//                           sequi consequatur magnam architecto iure sed tempora,
-//                           doloremque nam? Nesciunt, ad!
-//                           <button className="btn btn-primary d-block w-100 mt-2">
-//                             order
-//                           </button>
-//                         </div>
-//                       </div>
-//                     </div>
-//                   ))}
-//                 </div>
-//               </MenuBlock>
-//             ))}
-//           </Menu>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+  const onBlockIntersection = (e, index) => {
+    console.log(index, "onBlockIntersection");
+    setActiveTab(index);
+  };
+  return (
+    <>
+      <div
+        className="sticky-top bg-light"
+        style={{ position: "sticky", top: 0, background: "white", zIndex: 99 }}
+      >
+        <div className="container">
+          <Tabs activeTab={activeTab} onTabClick={onTabClick}>
+            {/* generating an array to loop through it  */}
+            {[...Array(20).keys()].map((item) => (
+              <Tab key={item}>Tab {item}</Tab>
+            ))}
+          </Tabs>
+        </div>
+      </div>
+      <div className="row mx-auto justify-content-center">
+        <div className="col-md-9 ">
+          <Menu
+            onBlockIntersection={onBlockIntersection}
+            // // containerClassName="container"
+            // indicatorTopPostion={60}
+            activeSection={activeTab}
+            action={menuRef}
+          >
+            {[...Array(20).keys()].map((item) => (
+              <MenuBlock key={item}>
+                <div className="display-4">Block {item}</div>{" "}
+                <div className="row">
+                  {[...Array(8).keys()].map((card) => (
+                    <div key={card} className="col-md-3 my-2">
+                      <div className="card">
+                        <div className="card-body">
+                          {card} Lorem ipsum dolor sit amet consectetur,
+                          adipisicing elit. Modi deleniti natus voluptates
+                          doloribus voluptate voluptas ab eum dolorem asperiores
+                          sequi consequatur magnam architecto iure sed tempora,
+                          doloremque nam? Nesciunt, ad!
+                          <button className="btn btn-primary d-block w-100 mt-2">
+                            order
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </MenuBlock>
+            ))}
+          </Menu>
+        </div>
+      </div>
+    </>
+  );
+}
 
-// export default App;
-
-import React from "react";
-
-const index = () => {
-  return <div>index</div>;
-};
-
-export default index;
+export default App;
